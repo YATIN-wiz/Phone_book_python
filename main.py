014e40d239c5099d633f641a470ebc4124c40bc1
Your Name <you@example.com>
2024-08-08 19:15:15 +0530

    searching and printing the contents

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def menu():
     print("2. ADD A NEW NUMBER")
     print("3. DELETE A NUMBER")
     print("4. SEARCH FOR A NUMBER")
+    print("5. EXIT")
 
     choice = int(input("Enter your choice (1-4): "))
     return choice
@@ -30,6 +31,15 @@ while True:
         cursor.execute("DELETE FROM CONTACTS WHERE Name =?", (name,))
         conn.commit()
         print("Number deleted successfully!")
+    elif choice == 4:
+        name = input("Enter name to be searched:")
+        cursor.execute("SELECT Name, Number FROM CONTACTS WHERE Name =?", (name,))
+        print(cursor.fetchall())
+    elif choice == 5:
+        print("Exiting...")
+        break
+    
+
     
    
 
